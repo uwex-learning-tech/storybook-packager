c2ffb782862c9daa5cb26e9fcbf59912d447739c
Ethan Lin <ethan.lin@uwex.edu>
2018-09-11 12:39:19 -0500

    Change xcode project to a document-based app

--- StorybookPackager/StorybookPackager/AppDelegate.swift
+++ StorybookPackager/StorybookPackager/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  Storybook Packager
+//  StorybookPackager
 //
-//  Created by Ethan Lin on 9/7/18.
+//  Created by Ethan Lin on 9/11/18.
 //  Copyright © 2018 University of Wisconsin System. All rights reserved.
 //
 
--- StorybookPackager/StorybookPackager/StartViewController.swift
+++ StorybookPackager/StorybookPackager/StartViewController.swift
@@ -1,38 +1,38 @@
 //
 //  ViewController.swift
-//  Storybook Packager
+//  StorybookPackager
 //
-//  Created by Ethan Lin on 9/7/18.
+//  Created by Ethan Lin on 9/11/18.
 //  Copyright © 2018 University of Wisconsin System. All rights reserved.
 //
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+class StartViewController: NSViewController {
+    
     @IBOutlet weak var recentProjectView: NSTableView!
     
     var recentProjects: Array<String> = ["ap340_lesson5", "smgt115_lesson2", "apc340_lesson5"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         recentProjectView.delegate = self
         recentProjectView.dataSource = self
         
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
+    
+    
 }
 
-extension ViewController: NSTableViewDataSource {
+extension StartViewController: NSTableViewDataSource {
     
     func numberOfRows(in tableView: NSTableView) -> Int {
         return recentProjects.count
@@ -40,7 +40,7 @@ extension ViewController: NSTableViewDataSource {
     
 }
 
-extension ViewController: NSTableViewDelegate {
+extension StartViewController: NSTableViewDelegate {
     
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
         
@@ -50,11 +50,7 @@ extension ViewController: NSTableViewDelegate {
         
         if ( tableColumn == recentProjectView.tableColumns[0] ) {
             
-            print("hit")
-            
             if let cell = recentProjectView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "projectCell"), owner: nil ) as? NSTableCellView {
-                
-                print("cellhit")
                 
                 cell.textField?.stringValue = recentProjects[row]
                 
@@ -63,9 +59,10 @@ extension ViewController: NSTableViewDelegate {
             }
             
         }
-
+        
         return nil
         
     }
     
 }
+
