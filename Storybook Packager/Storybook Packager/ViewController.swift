ddaba38a1a9726bd4c14ef01d92b0dadc019ec1f
Ethan Lin <ethan.lin@uwex.edu>
2018-09-11 09:58:59 -0500

    Added Start screen

--- Storybook Packager/Storybook Packager/ViewController.swift
+++ Storybook Packager/Storybook Packager/ViewController.swift
@@ -10,10 +10,17 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var recentProjectView: NSTableView!
+    
+    var recentProjects: Array<String> = ["ap340_lesson5", "smgt115_lesson2", "apc340_lesson5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        recentProjectView.delegate = self
+        recentProjectView.dataSource = self
+        
     }
 
     override var representedObject: Any? {
@@ -25,3 +32,40 @@ class ViewController: NSViewController {
 
 }
 
+extension ViewController: NSTableViewDataSource {
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return recentProjects.count
+    }
+    
+}
+
+extension ViewController: NSTableViewDelegate {
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        
+        if ( recentProjects.count <= 0 ) {
+            return nil
+        }
+        
+        if ( tableColumn == recentProjectView.tableColumns[0] ) {
+            
+            print("hit")
+            
+            if let cell = recentProjectView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "projectCell"), owner: nil ) as? NSTableCellView {
+                
+                print("cellhit")
+                
+                cell.textField?.stringValue = recentProjects[row]
+                
+                return cell
+                
+            }
+            
+        }
+
+        return nil
+        
+    }
+    
+}
