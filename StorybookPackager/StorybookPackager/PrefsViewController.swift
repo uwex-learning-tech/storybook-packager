8082aeab35b6738c3088781d966fa8479c5c7472
Ethan Lin <ethan.lin@uwex.edu>
2018-11-07 11:37:01 -0600

    added preference view controller

---
+++ StorybookPackager/StorybookPackager/PrefsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PrefsViewController.swift
+//  Storybook Packager
+//
+//  Created by Ethan Lin on 11/7/18.
+//  Copyright © 2018 University of Wisconsin System. All rights reserved.
+//
+
+import Cocoa
+
+class PrefsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
