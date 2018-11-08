8a0e717492e3043868a25f947363960e1aacd0e9
Ethan Lin <ethan.lin@uwex.edu>
2018-11-08 11:59:05 -0600

    connect new presentation to NSDocument instead of AppDelegate

--- StorybookPackager/StorybookPackager/AppDelegate.swift
+++ StorybookPackager/StorybookPackager/AppDelegate.swift
@@ -10,7 +10,7 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         
         // install an app directory in User > Library > Application Support
@@ -42,25 +42,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return false
     }
     
-    @IBAction func newPresenation(_ sender: NSMenuItem) {
-
-        let startPanel = NSApp.keyWindow
-
-        if ( startPanel != nil ) {
-            
-            if ( startPanel?.identifier?.rawValue == SegueIdentifiers.start ) {
-                
-                startPanel?.close()
-                
-            }
-            
-        }
-        
-        let newPresentation = NSStoryboard(name: StoryboardIdentifiers.main, bundle: nil).instantiateController(withIdentifier: SegueIdentifiers.presentation) as! NSWindowController
-        
-        newPresentation.window?.windowController?.showWindow(nil)
-        newPresentation.window?.makeMain()
-        
-    }
 }
 
