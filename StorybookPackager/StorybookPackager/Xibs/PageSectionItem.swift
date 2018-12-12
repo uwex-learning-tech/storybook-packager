3fd7a6e0f36adbea5e1b9b33f5125eb0d387a5cd
Ethan Lin <ethan.lin@uwex.edu>
2018-12-12 16:26:26 -0600

    added files for page section item

---
+++ StorybookPackager/StorybookPackager/Xibs/PageSectionItem.swift
@@ -0,0 +1,18 @@
+//
+//  PageSectionItem.swift
+//  Storybook Packager
+//
+//  Created by Ethan Lin on 12/12/18.
+//  Copyright © 2018 University of Wisconsin System. All rights reserved.
+//
+
+import Cocoa
+
+class PageSectionItem: NSCollectionViewItem {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
