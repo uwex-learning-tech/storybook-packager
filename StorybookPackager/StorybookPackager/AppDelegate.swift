75241b486b08c975bea0c1920d5eceae26d52819
Ethan Lin <ethan.lin@uwex.edu>
2018-12-13 12:10:17 -0600

    added touch bar items for start window

--- StorybookPackager/StorybookPackager/AppDelegate.swift
+++ StorybookPackager/StorybookPackager/AppDelegate.swift
@@ -24,6 +24,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // show start panel
         showStartPanel()
         
+        // allow Touch Bar customization
+        if #available(OSX 10.12.2, *) {
+            NSApplication.shared.isAutomaticCustomizeTouchBarMenuItemEnabled = true
+        }
+        
     }
     
     func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
