cdcc3a5a376e7c2bc84e4c800f30898934674ee6
Ethan Lin <ethan.lin@uwex.edu>
2019-03-05 14:11:32 -0600

    prevent app from opening untitled document after crash

--- StorybookPackager/StorybookPackager/AppDelegate.swift
+++ StorybookPackager/StorybookPackager/AppDelegate.swift
@@ -33,6 +33,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
     }
     
+    func applicationShouldOpenUntitledFile(_ sender: NSApplication) -> Bool {
+        return false
+    }
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
