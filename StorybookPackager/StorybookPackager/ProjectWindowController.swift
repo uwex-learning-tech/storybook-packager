f7ded7bf9f8ef794cac8843ef78aacdc07b919ef
Ethan Lin <ethan.lin@uwex.edu>
2021-11-17 17:17:14 -0600

    fixed no internet icon

--- StorybookPackager/StorybookPackager/ProjectWindowController.swift
+++ StorybookPackager/StorybookPackager/ProjectWindowController.swift
@@ -17,7 +17,6 @@ class ProjectWindowController: NSWindowController {
     @IBOutlet weak var touchBarConfirmTitleBtn: NSButton!
     
     let reachability = try! Reachability()
-    var accessoryViewController: NSTitlebarAccessoryViewController?
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -26,13 +25,11 @@ class ProjectWindowController: NSWindowController {
     
     override func windowDidLoad() {
         super.windowDidLoad()
-    
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
         
         accessoryView.isHidden = true
         
         // load accessory view
-        createAccessoryViewControllerIfNeeded()
+        createAccessoryViewController()
         
         // check for internet connetion
         NotificationCenter.default.addObserver(self, selector: #selector(reachabilityChanged(note:)), name: .reachabilityChanged, object: reachability)
@@ -51,9 +48,8 @@ class ProjectWindowController: NSWindowController {
     @objc func reachabilityChanged(note: Notification) {
         
         let reachability = note.object as! Reachability
-        
         switch reachability.connection {
-        case .none:
+        case .unavailable:
             self.accessoryView.isHidden = false
         default:
             self.accessoryView.isHidden = true
@@ -105,12 +101,9 @@ class ProjectWindowController: NSWindowController {
         windowTitleFld.stringValue = with
     }
     
-    fileprivate func createAccessoryViewControllerIfNeeded() {
+    fileprivate func createAccessoryViewController() {
         
-        guard self.accessoryViewController == nil else { return }
-
         let accessoryViewController = NSTitlebarAccessoryViewController()
-        self.accessoryViewController = accessoryViewController
         accessoryViewController.view = accessoryView
         accessoryViewController.layoutAttribute = .right
         self.window?.addTitlebarAccessoryViewController(accessoryViewController)
